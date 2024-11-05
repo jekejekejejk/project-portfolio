19aef71da2befa84d6eba2a33d1354b97bf65f90
jekejekejejk <amoresjesekiel@gmail.com>
2024-11-05 18:58:38 +0800

    Add files via upload

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          bebas: ['Bebas Neue'], // You can choose a fallback font
+        },
+      },
+    },
+    plugins: [],
+  };
